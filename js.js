70af9ee52bb9fde8176d6a4be85b7f946bdc287c
AlexisEscobales <aescobalesg2122@politecnics.barcelona>
2023-05-05 20:35:56 +0200

    quien soy acabado menos el video

--- js.js
+++ js.js
@@ -3,52 +3,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
         var header = document.querySelector("header");
         header.classList.toggle("sticky", window.scrollY > 0);
     })
-
-  
-
+      
 });
-
-  function openPopup() {
-        document.getElementById("popup1").classList.toggle("active");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //SCROLL LENTAMENTE
-// function scrollDown() {
-//   const scrollHeight = document.documentElement.scrollHeight;
-//   const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-//   const scrollBottom = scrollHeight - window.innerHeight - scrollTop;
-//   const scrollStep = Math.max(scrollBottom / 200, 200);
-
-//   function doScroll() {
-//     window.scrollBy(0, scrollStep);
-
-//     const reachedBottom = (scrollTop + window.innerHeight) >= scrollHeight;
-
-//     if (!reachedBottom) {
-//       requestAnimationFrame(doScroll);
-//     }
-//   }
-
-//   doScroll();
-// }
