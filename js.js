467052edeac0c335eef2db2d6a643b245eac3879
Alexis Escobales <aescobalesg2122@politecnics.barcelona>
2023-05-17 13:34:52 +0200

    chaneg logo

--- js.js
+++ js.js
@@ -1,7 +1,34 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     window.addEventListener("scroll", function () {
+//         var header = document.querySelector("header");
+//         header.classList.toggle("sticky", window.scrollY > 0);
+//         logo = document.querySelector('#logo');
+//         logo.style.backgroundImage = "url(/logoblanco.svg)";
+//         if (header.classList.contains("sticky")) {
+//              logo.classList.add('logoN')
+//         } else {
+//             logo.style.backgroundImage = "url(/logoblanco.svg)";
+//         }
+//     })
+// });
+
 document.addEventListener("DOMContentLoaded", (event) => {
+    var header = document.querySelector("header");
+    var logo = document.querySelector('#logo');
+  
+    if (window.scrollY > 0) {
+        header.classList.add("sticky");
+    }
+
     window.addEventListener("scroll", function () {
-        var header = document.querySelector("header");
         header.classList.toggle("sticky", window.scrollY > 0);
-    })
-      
+        
+        if (header.classList.contains("sticky")) {
+            logo.classList.remove('logo');
+            logo.classList.add('logoN');
+        } else {
+            logo.classList.remove('logoN');
+            logo.classList.add('logo');
+        }
+    });
 });
